f8f151658f20d295053e35a5c802bbe2089a0de8
ViktorD <vdimitrenko96@gmail.com>
2022-12-20 19:28:45 +0700

    Initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,11 @@
+public class CreditPaymentService {
+    public int calculate(int yearsOfLoan) {
+        float annualPercent = 9.99F;
+        int loanMoneyAmount = 1_000_000;
+        float monthlyRate = annualPercent / 100 / 12; // Ставка в месяц
+        double monthlyPay = loanMoneyAmount * ((monthlyRate * Math.pow((1 + monthlyRate), 12 * yearsOfLoan)) / (Math.pow((1 + monthlyRate), 12 * yearsOfLoan) - 1));
+        double totalLoanPayment = 12 * yearsOfLoan * monthlyPay;
+        return (int) monthlyPay;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        System.out.println();
+        System.out.println("Ежемесячный платёж. 1 год");
+        System.out.println(service.calculate(1));
+
+        System.out.println();
+        System.out.println("Ежемесячный платёж. 2 года");
+        System.out.println(service.calculate(2));
+        
+        System.out.println();
+        System.out.println("Ежемесячный платёж. 3 года");
+        System.out.println(service.calculate(3));
+    }
+}
